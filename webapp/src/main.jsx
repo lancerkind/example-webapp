9ae07b7ac39c743ea5ee66ff53a07edd140a5ea1
Lance Kind <lancerkind@yahoo.com>
2025-12-01 21:37:18 -0600

    trying out dynamic afterSignInUrl.

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -10,7 +10,8 @@ createRoot(document.getElementById("root")).render(
     <AsgardeoProvider
       clientId="2zUGAC1fmUqGV7yN010RvXoyLu0a"
       baseUrl="https://api.asgardeo.io/t/harshareactdemo"
-      afterSignInUrl="http://asgardeo-webapp-demo-env.eba-22drhbvm.us-east-1.elasticbeanstalk.com/tours"
+      //afterSignInUrl="http://asgardeo-webapp-demo-env.eba-22drhbvm.us-east-1.elasticbeanstalk.com/tours"
+      afterSignInUrl={window.location.origin + "/tours"}
       tokenValidation={{ idToken: { validate: false } }}
     >
       <App></App>
